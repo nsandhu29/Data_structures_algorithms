5602efc67411ac65df77478cca82874397521421
Navjot Singh <nsandhu0001@gmail.com>
2022-01-18 22:52:06 -0600

    Using decorator to calculate runtime

--- edx_DS_ALGO/GCD.py
+++ edx_DS_ALGO/GCD.py
@@ -1,6 +1,17 @@
-import timeit
+import functools
+from timeit import default_timer as timer
 
+def elapsed_time(func):
+    functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start = timer()
+        result = func(*args, **kwargs)
+        end = timer()
+        print(f'Runtime for {func.__name__} is {end - start}')
+        return result
+    return wrapper
 
+@elapsed_time
 def naive_gcd(a, b):
     gcd = 0
     for d in range(1, a + b):
@@ -8,21 +19,20 @@ def naive_gcd(a, b):
             gcd = d
     return gcd
 
+
+@elapsed_time
 def euclidean_gcd(a, b):
-    if b == 0:
-        return a
-    a_remainder = a % b
-    return euclidean_gcd(b, a_remainder)
+    def _euclidean_gcd(a, b):
+        if b == 0:
+            return a
+        a_remainder = a % b
+        return _euclidean_gcd(b, a_remainder)
+    return _euclidean_gcd(a, b)
 
 if __name__ == "__main__":
     a = 1653264
     b = 3918848
-    start = timeit.default_timer()
-    gcd = naive_gcd(a, b)
-    end = timeit.default_timer()
-    print(f'{end - start} to compute gcd of {a}, {b} = {gcd}')
-    start1 = timeit.default_timer()
-    gcd1 = euclidean_gcd(a, b)
-    end1 = timeit.default_timer()
-    print(f'{end1 - start1} to compute gcd of {a}, {b} = {gcd1}')
+    naive_gcd(a, b)
+    euclidean_gcd(a, b)
+    
     
--- edx_DS_ALGO/fibonacci.py
+++ edx_DS_ALGO/fibonacci.py
@@ -1,18 +1,33 @@
-import timeit
+import functools
+from timeit import default_timer as timer
+
+def run_time(func):
+    functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start = timer()
+        result = func(*args, **kwargs)
+        stop = timer()
+        print(f'Run time of {func.__name__} is {stop - start} seconds')
+        return result
+    return wrapper
 
 
+@run_time
 def fib_recur(n):
     """
     Simple Recurssive algorithm to calculate fibonacci
     It is not efficient as run time is too long
     """
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fib_recur(n -1) + fib_recur(n -2)
+    def _fib_recur(n):
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        else:
+            return _fib_recur(n -1) + _fib_recur(n -2)
+    return _fib_recur(n)
 
+@run_time
 def fib_efficient(n):
     """
     Algorithm to calculate fibinacci using list.
@@ -25,15 +40,18 @@ def fib_efficient(n):
         ls.append(num)
     return ls[n]
 
-start1 = timeit.default_timer()
-fib_n1 = fib_recur(50)
-end1 = timeit.default_timer()
-print(f'Fibbonaci recur {fib_n1} took time {end1 - start1}')
+# start1 = timeit.default_timer()
+# fib_n1 = fib_recur(50)
+# end1 = timeit.default_timer()
+# print(f'Fibbonaci recur {fib_n1} took time {end1 - start1}')
 
-start = timeit.default_timer()
-fib_n = fib_efficient(100000)
-end = timeit.default_timer()
-print(f'Fibbonaci effective {fib_n} took time {end - start}')
+# start = timeit.default_timer()
+# fib_n = fib_efficient(100000)
+# end = timeit.default_timer()
+# print(f'Fibbonaci effective {fib_n} took time {end - start}')
+if __name__ == "__main__":
+    fib_recur(30)
+    fib_efficient(30)
 
 
 
